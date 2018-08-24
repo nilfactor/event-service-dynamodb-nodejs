90320af461d5c367bf9e44a300ac25d04ac56629
Richard Williamson <rwilliamson@mednetstudy.com>
2018-08-24 15:03:10 -0500

    updates

--- __test__/handler.test.js
+++ __test__/handler.test.js
@@ -1,15 +1,55 @@
+const aws = require('aws-sdk-mock');
+const realAws = require('aws-sdk');
 const object = require('../handler');
+
+aws.setSDKInstance(realAws);
+process.env.NO_LAMBDA_LOG = true;
 
 describe('handler', async () => {
     test('gives an error when data does not meet expected', async () => {
-        let error;
+        const expectedError = new Error('event not as expected');
+        await expect(object.run()).rejects.toThrowError(expectedError);
+    });
 
-        try {
-            await object.run();
-        } catch (e) {
-            error = e;
-        }
+    test('gives an error when malformed body', async () => {
+        const data = '{"test": test}';
+        const expectedError = new Error('json body malformed');
+        await expect(object.run({ body: data })).rejects.toThrowError(expectedError);
+    });
 
-        expect(error).toEqual(new Error('event not as expected'));
+    test('gives an error when dynamodb fails', async () => {
+        const data = {
+            eventId: '12345-test',
+            dateCreate: '2018-09-07 13:37:00',
+            data: 'test data',
+        };
+        const mockError = new Error('simulated error');
+        const expectedError = new Error('dynamodb error');
+        const mockClient = async (params) => {
+            expect(params.Item).toEqual(data);
+            throw mockError;
+        };
+
+        aws.mock('DynamoDB.DocumentClient', 'put', mockClient);
+        await expect(object.run({ body: JSON.stringify(data) })).rejects.toThrowError(expectedError);
+        aws.restore('DynamoDB.DocumentClient', 'put');
+    });
+
+    test('retuns as expected when successful', async () => {
+        const data = {
+            eventId: '12345-test',
+            dateCreate: '2018-09-07 13:37:00',
+            data: 'test data',
+        };
+
+        const mockClient = async (params) => {
+            expect(params.Item).toEqual(data);
+            return params;
+        };
+        await aws.mock('DynamoDB.DocumentClient', 'put', mockClient);
+        const result = await object.run({ body: JSON.stringify(data) });
+        expect(result.TableName).toBe('event-service-db');
+        expect(result.Item).toEqual(data);
+        aws.restore('DynamoDB.DocumentClient', 'put');
     });
 });
--- handler.js
+++ handler.js
@@ -1,5 +1,42 @@
+const aws = require('aws-sdk');
+
+const region = process.env.AWS_REGION || 'us-east-1';
+const TableName = 'event-service-db';
+
+const log = (type, what) => {
+    // istanbul ignore if
+    if (typeof process.env.NO_LAMBDA_LOG === 'undefined') {
+        console[type](what);
+    }
+};
+
 module.exports.run = async (event) => {
-    if (typeof event === 'undefined' || typeof event.body !== 'undefined') {
+    let data;
+
+    if (typeof event === 'undefined' || typeof event.body === 'undefined') {
+        log('error', 'event not as expected');
         throw new Error('event not as expected');
+    }
+
+    try {
+        data = JSON.parse(event.body);
+    } catch (e) {
+        log('error', e);
+        throw new Error('json body malformed');
+    }
+
+    const params = {
+        TableName,
+        Item: data,
+    };
+
+    try {
+        const ddb = new aws.DynamoDB.DocumentClient({ region });
+        const result = await ddb.put(params).promise();
+        log('log', result);
+        return result;
+    } catch (e) {
+        log('error', e);
+        throw new Error('dynamodb error');
     }
 };
